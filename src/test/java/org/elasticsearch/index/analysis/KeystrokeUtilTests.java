933aa685a98497c241f4febd92beb63727eaaa55
Masaru Hasegawa <haniomasali@gmail.com>
2017-05-25 13:50:39 +0900

    Add test for keystroke mapping

--- src/test/java/org/elasticsearch/index/analysis/KeystrokeUtilTests.java
+++ src/test/java/org/elasticsearch/index/analysis/KeystrokeUtilTests.java
@@ -4,7 +4,6 @@ package org.elasticsearch.index.analysis;
 import org.elasticsearch.test.ESTestCase;
 
 import java.util.HashSet;
-import java.util.Map;
 
 import static java.util.stream.Collectors.toList;
 import static org.hamcrest.Matchers.hasSize;
@@ -12,11 +11,17 @@ import static org.hamcrest.Matchers.hasSize;
 public class KeystrokeUtilTests extends ESTestCase {
 
     public void testMappingHasNoDuplicates() {
-        KeystrokeUtil.KEY_STROKE_MAP.entrySet().stream()
-                .map(Map.Entry::getValue)
+        KeystrokeUtil.KEY_STROKE_MAP.values().stream()
                 .map(l -> l.stream().map(Keystroke::getKey).collect(toList()))
                 .forEach(l -> {
                     assertThat("Duplicate in entry:" + l, l, hasSize(new HashSet<>(l).size()));
                 });
+    }
+
+    public void testMappingHasSingleHistory() {
+        KeystrokeUtil.KEY_STROKE_MAP.values().forEach(l -> l.forEach(ks -> {
+            System.out.println(ks);
+            assertThat("weight history > 1" + l, ks.getWeightHistory(), hasSize(1));
+                }));
     }
 }
